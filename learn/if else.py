6e8b980e78c8087eb8f387999dca39c9f83b3e5c
viswa <viswa.r2002@gmail.com>
2024-08-12 20:06:47 +0530

    new

---
+++ learn/if else.py
@@ -0,0 +1,20 @@
+#Boolean values(True False)
+if (False):
+    print("yes")
+else:
+    print("no")    
+
+    #Comparison operator
+print("win"=="win")
+#if or else any one of the block will work accordingly to the condition.
+rcb="win"
+if(rcb=="win"):
+    print("isal cup namdhe")
+else:
+    print("no cup")    
+
+meghana=input("megana died or not?")
+if(meghana=="died"):
+    print("Surya meets priya")
+else:
+    print("Surya meets meghna")
